33ad740a87d09eac7bedcb50dffafad7db50bec9
Memeladon <bingorussian@mail.ru>
2023-09-26 15:27:42 +0300

    check

--- main.py
+++ main.py
@@ -42,9 +42,10 @@ templates = Jinja2Templates(directory="frontend/templates")
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
+
 # --------------------------------------------- Get Post ---------------------------------------------- #
+
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
