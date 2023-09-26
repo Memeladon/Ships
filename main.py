a1c9e81552d20005f8f5adff52fdaf9deee27851
Memeladon <bingorussian@mail.ru>
2023-09-27 00:24:06 +0300

    web exp

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ Cross Origin Resource Sharing (CORS). Встроенный в FastAPI CORSMiddle
 
 origins = [
     "http://localhost:3000",
+    "127.0.0.1:8000",
     "localhost:3000"
 ]
 
@@ -45,7 +46,7 @@ if __name__ == "__main__":
 
 # --------------------------------------------- Get Post ---------------------------------------------- #
 
-
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
