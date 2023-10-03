6e29131297d545bb3ecb1223e9b19d90ec27c39a
Михаил Горбань <91434795+usernameisavailablee@users.noreply.github.com>
2023-10-03 23:33:06 +0300

    skillet

--- test_class.py
+++ test_class.py
@@ -1,13 +1,33 @@
 from backend.src.classes.matrix_processing import MatrixProcessing
 
-def turn(who_is_turn):
-    if who_is_turn == "player":
-        pass
+import random
+def lets_game(player,bot):
 
+    random_variable_for_turn = randint(0,1)
+    if randint == 0:
+        turn = "player"
     else:
+        turn = "bot"
 
-        pass
-    pass
+    while ((player.end_of_game() != True) or (bot.end_of_game() != True)):
+    if turn == "player":
+        # принятие координат с веба (GET)
+        bot.attack(]принятые координаты атаки c web])
+        if (bot.attack(]принятые координаты атаки c web]) == "Попал!") or  (bot.attack() == "Разбил!"):
+            turn = "player"
+        #публикация матрицы(POST)
+    else:
+        #отправка матрицы прологу
+        #принятие координат от пролога
+        player.attack(]принятые координаты с prolog])
+        if (player.attack(]принятые координаты с prolog]) == "Попал!") or  (bot.attack() == "Разбил!"):
+            turn = "bot"
+        #публикация матрицы(POST)
+
+    if player.end_of_game():
+        game_result = "Вы проиграли"
+    else:
+        game_result = "Победа!"
 
 
 amount_types = 4
@@ -17,17 +37,22 @@ battleground_columns = 10
 bot = MatrixProcessing(None,battleground_rows,battleground_columns)
 bot.random_place(amount_types)
 
+#сюда отправить, сам ли располагает
 random_place = True
-if random_place == True:
+if random_place:
     player = MatrixProcessing(None,10,10)
     player.random_place(amount_types)
 else:
+    #сюда подтянуть матрицу с кораблями, в случае если матрица кастомная 
     player = MatrixProcessing(customized_matrix)
     pass
 
-# while (True):
 
-#     pass
+
+
+
+
+
 
 
 
