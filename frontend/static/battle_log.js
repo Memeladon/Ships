32eb7033ed3c7a943eb5f77f8e5c964e5fa8a745
Niksa7 <nikitasoroka228@gmail.com>
2023-10-08 21:41:46 +0300

    Добавлен лог боя и start game button

---
+++ frontend/static/battle_log.js
@@ -0,0 +1,39 @@
+function addPlacementLog(message) {
+    const now = new Date();
+    const formattedTime = now.getHours().toString() + ':' + now.getMinutes().toString() + ':' + now.getSeconds().toString();
+
+    const logContainer = document.querySelector('.log-message');
+    const logEntry = document.createElement('div');
+    logEntry.textContent = `[${formattedTime}] ${message}`;
+    logContainer.appendChild(logEntry);
+
+    logContainer.scrollTop = logContainer.scrollHeight;  // автоматическая прокрутка вниз
+}
+
+function addBattleLog(playerName, x, y, status) {
+    const now = new Date();
+    const formattedTime = now.getHours().toString().padStart(2, '0') + ':' + now.getMinutes().toString() + ':' + now.getSeconds().toString();
+    const logContainer = document.querySelector('.log-message');
+
+    let message;
+    switch (status) {
+        case 8:
+            message = `Клетка (${x}, ${y}). Корабль поврежден!`;
+            break;
+        case 7:
+            message = `Клетка (${x}, ${y}). Корабль потоплен!`;
+            break;
+        case 1:
+            message = `Клетка (${x}, ${y}). Промах!`;
+            break;
+        default:
+            message = `Неизвестное действие на клетке (${x}, ${y}).`;
+            break;
+    }
+
+    const logEntry = document.createElement('div');
+    logEntry.textContent = `[${formattedTime}] ${playerName}: ${message}`;
+    logContainer.appendChild(logEntry);
+
+    logContainer.scrollTop = logContainer.scrollHeight;  // автоматическая прокрутка вниз
+}
